7ce94d523e60079f8e555056944b4a795f8b2a7b
Dharan Aditya <dharan.aditya@outlook.com>
2021-03-07 23:49:59 +0530

    push++

--- prob.py
+++ prob.py
@@ -4,5 +4,5 @@ for i in range(0, n):
     c = text[i]
     if(c == 'a' or c == 'e' or c == 'i' or c == 'o' or c == 'u'):
         print("#", end = '')
-    else
+    else:
         print("$", end = '')
